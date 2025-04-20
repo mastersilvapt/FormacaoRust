13e91a0107afc50ebb1a2f7359d1e7992e35cfbc
Andre Silva <andresilva.aer@gmail.com>
2025-04-20 21:27:12 +0100

    Libraria e Mercearia v1

--- Structs/livraria/src/main.rs
+++ Structs/livraria/src/main.rs
@@ -1,4 +1,196 @@
+use std::fmt::Display;
+use std::io::{stdin, stdout, Write};
+use std::ops::Add;
+
+struct Library {
+    lib: Vec<(Book, u32)>,
+    requested: Vec<(Book, String)>
+}
+
+impl Library {
+    fn add_book(&mut self, book: Book) {
+        for bk in self.lib.iter_mut() {
+            if bk.0.isbn == book.isbn {
+                bk.1 += 1;
+                return;
+            }
+        }
+        self.lib.push((book, 1));
+    }
+
+    fn delete_book(&mut self, isbn: &String) {
+        let mut idx: Option<usize> = None;
+        for (i, bk) in self.lib.iter_mut().enumerate() {
+            if bk.0.isbn == *isbn {
+                bk.1 -= 1;
+                if bk.1 == 0 {
+                    idx = Some(i);
+                }
+                break;
+            }
+        }
+        if  let Some(idx) = idx {
+            self.lib.remove(idx);
+        }
+    }
+
+    fn list_books(&self) -> Vec<(Book, u32)> {
+        self.lib.clone()
+    }
+
+    fn help(&self) {
+        println!("Oprations:");
+        println!("add");
+        println!("remove");
+        println!("list");
+        println!("request");
+        println!("return");
+    }
+    fn apply(&mut self, op: &String) -> bool {
+        match op.as_ref() {
+            "add" => {
+                
+                let isbn = read_valid_stdin("ISBN>", |input| Ok(input.trim().to_string()));
+                let title = read_valid_stdin("Title>", |input| Ok(input.trim().to_string()));
+                let author = read_valid_stdin("Author>", |input| Ok(input.trim().to_string()));
+                let keywords = read_valid_stdin("Keywords (separated by comma)>", |input| Ok(input.trim().to_string()));
+                let keywords: Vec<String> = keywords.split(",")
+                    .map(|s| s.to_string()).collect::<Vec<String>>();
+                
+                let book = Book { isbn, title, author, keywords };
+                
+                if book.isbn == *"" || book.title == *"" ||
+                    book.author == *"" || book.keywords.is_empty() {
+                    return false;
+                }
+                self.add_book(book);
+                true
+            },
+            "remove" => {
+                let isbn = read_valid_stdin("ISBN>", |input| Ok(input.trim().to_string()));
+                
+                let count = match &self.list_books().iter()
+                    .find_map(|(b,c)| if b.isbn == isbn { Some(c) } else { None }){
+                    None => return false,
+                    Some(0) => return false,
+                    Some(c) => **c,
+                };
+                self.delete_book(&isbn);
+                self.list_books().iter().filter(|(b, c)| b.isbn == isbn && *c == count-1).count() == 1
+            },
+            "list" => {
+                println!("List ({}):", self.list_books().len());
+                for (bk,i) in self.list_books(){
+                    println!("{bk} | Number of books -> {i}");
+                }
+                
+                println!("Requested ({}):", self.requested.len());
+                for (i, (bk, name)) in self.requested.iter().enumerate() {
+                    println!("{}. {bk} - {name}", i+1);
+                }
+                true
+            },
+            "request" => {
+                let isbn = read_valid_stdin("ISBN>", |input| Ok(input.trim().to_string()));
+                let name = read_valid_stdin("NAME>", |input| Ok(input.trim().to_string()));
+                
+                let book = self.lib.iter_mut().find(|(x,n)| x.isbn == isbn);
+                if book.is_none(){
+                    return false;
+                }
+                let book = book.unwrap();
+                if book.1 == 0 {
+                    return false;
+                }
+                book.1 -= 1;
+                
+                self.requested.push((book.0.clone(), name));
+                
+                true
+            },
+            "return" => {
+                let number = read_valid_stdin("ID>", |input| {
+                    let input = input.trim();
+                    let number = input.parse::<usize>().map_err(|_| "Invalid ID")?;
+                    if number == 0 || number > self.requested.len() {
+                        Err("Out of Bounds")
+                    }else {
+                        Ok(number)
+                    }
+                });
+                
+                let request = self.requested.remove(number-1);
+                
+                let book = self.lib.iter_mut().find(|(x, _n)| x.isbn == request.0.isbn);
+                if book.is_none(){
+                    return false;
+                }
+                let book = book.unwrap();
+                book.1 += 1;
+                
+                true
+            }
+            _ => {
+                false
+            }
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+struct Book {
+    isbn: String,
+    title: String,
+    author: String,
+    keywords: Vec<String>,
+}
+
+impl Display for Book {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let str = String::from("ISBN: ")
+            .add(&self.isbn.clone())
+            .add(" | TITLE: ")
+            .add(&self.title.clone())
+            .add(" | AUTHOR: ")
+            .add(&self.author.clone());
+        write!(f, "{}", str)
+    }
+}
 
 fn main() {
-    todo!()
+    let mut library = Library { lib: vec![], requested: vec![] };
+    
+    library.add_book(Book { isbn: "123".to_string(), title: "Teste123".to_string(), author: "Andre Silva".to_string(), keywords: vec!["teste1".to_string(), "teste2".to_string()]});
+    library.add_book(Book { isbn: "123".to_string(), title: "Teste123".to_string(), author: "Andre Silva".to_string(), keywords: vec!["teste1".to_string(), "teste2".to_string()]});
+    library.add_book(Book { isbn: "234".to_string(), title: "Teste234".to_string(), author: "Filipe Silva".to_string(), keywords: vec!["teste3".to_string(), "teste4".to_string()]});
+    library.add_book(Book { isbn: "345".to_string(), title: "Teste345".to_string(), author: "Jose Silva".to_string(), keywords: vec!["teste5".to_string(), "teste6".to_string()]});
+    library.add_book(Book { isbn: "345".to_string(), title: "Teste345".to_string(), author: "Jose Silva".to_string(), keywords: vec!["teste5".to_string(), "teste6".to_string()]});
+    library.add_book(Book { isbn: "345".to_string(), title: "Teste345".to_string(), author: "Jose Silva".to_string(), keywords: vec!["teste5".to_string(), "teste6".to_string()]});
+    
+    let mut input: String;
+    loop {
+        library.help();
+        input = read_valid_stdin(">", |input| Ok(input.trim().to_string()));
+
+        if input.eq_ignore_ascii_case("exit") {
+            break;
+        }
+        library.apply(&input);
+    }
+}
+
+fn read_valid_stdin<F, T>(prompt: &str, maplidator: F) -> T
+where
+    F: Fn(String) -> Result<T, &'static str>,
+{
+    loop {
+        let mut input = String::new();
+        print!("{}", prompt);
+        stdout().flush().unwrap();
+        stdin().read_line(&mut input).unwrap();
+        match maplidator(input) {
+            Ok(value) => break value,
+            Err(msg) => println!("{}", msg),
+        }
+    }
 }
--- Structs/mercearia/src/main.rs
+++ Structs/mercearia/src/main.rs
@@ -1,3 +1,256 @@
+use std::io::Write;
+
+#[derive(Clone, Debug)]
+struct Product {
+    id: u32,
+    name: String,
+    expiration: String,
+    price: f64,
+    quantity: usize,
+}
+
+// Fileira -> Prateleira -> Zona
+enum Command {
+    AddProduct(usize, usize, usize, Product),
+    Resupply(u32, usize),
+    RemoveProduct(u32, usize),
+    ChangePrice(u32, f64),
+    MoveTo(u32, usize, usize, usize),
+    Show(),
+}
+
+#[derive(Clone, Debug)]
+struct Supermarket {
+    products: Vec<Vec<Vec<Option<Product>>>>,
+}
+
+impl Supermarket {
+    fn new() -> Self {
+        let  products: Vec<Vec<Vec<Option<Product>>>> = vec![vec![vec![]]];
+        Supermarket { products }
+    }
+
+    fn find_coords(&self, id: u32) -> Result<(usize, usize, usize), &'static str> {
+        for (i, fileira) in self.products.iter().enumerate() {
+            for (j, prateleira) in fileira.iter().enumerate() {
+                for(k, opt) in prateleira.iter().enumerate() {
+                    match opt {  
+                        Some(prod) => {
+                           if(prod.id == id) {
+                               return Ok((i, j, k));
+                           } 
+                        }
+                        None => {
+                            continue;
+                        }
+                    }
+                    
+                }
+            }
+        }
+        Err("Product not found")
+    }
+    
+    fn make_position(&mut self, (i, j, k): (usize, usize, usize)) {
+        while self.products.len() <= i {
+            self.products.push(vec![]);
+        }
+        while self.products[i].len() <= j {
+            self.products[i].push(vec![]);
+        }
+        self.products[i][j].resize(k+1, None);
+    }
+    
+    fn apply(&mut self, command: Command) -> Result<&str, &'static str>{
+        match command {
+            Command::AddProduct(fileira, prateleira, zone, product) => {
+                match self.find_coords(product.id){
+                    Ok(_) => {
+                         Err("Product already exists")          
+                    }
+                    Err(_) => {
+                        self.make_position((fileira, prateleira, zone));
+                        self.products[fileira][prateleira][zone] = Some(product);
+                        Ok("Added product")
+                    }
+                }
+            }
+            Command::Resupply(id, quantity) => {
+                let (i,j,k) = self.find_coords(id)?;
+                let product = &mut self.products[i][j][k];
+                if product.is_some() {
+                    product.as_mut().unwrap().quantity += quantity;
+                    Ok("Resupplied")
+                }else { 
+                    Err("Product not found")
+                }
+            }
+            Command::RemoveProduct(id, quantity) => {
+                let (i,j,k) = self.find_coords(id)?;
+                let product = &mut self.products[i][j][k];
+                if product.as_mut().unwrap().quantity < quantity { 
+                    return Err("Cannot remove more than in ");
+                }
+                if product.as_mut().unwrap().quantity - quantity == 0 {
+                    self.products[i][j].remove(k);
+                }else {
+                    product.as_mut().unwrap().quantity -= quantity;
+                }
+                Ok("Product Removed")
+            }
+            Command::ChangePrice(id, new_price) => {
+                let (i,j,k) = self.find_coords(id)?;
+                let product = &mut self.products[i][j][k];
+                product.as_mut().unwrap().price = new_price;
+                Ok("New price set")
+            }
+
+            Command::MoveTo(id, fileira, prateleira, zone) => {
+                let (i, j, k) = self.find_coords(id)?;
+                if self.products[fileira][prateleira][zone].is_some(){
+                    return Err("Product already occupied");
+                }
+
+                let product = self.products[i][j].remove(k);
+                self.make_position((fileira, prateleira, zone));
+                self.products[fileira][prateleira][zone] = product;
+                
+                Ok("Product moved")
+            }
+            
+            Command::Show() => {
+                self.products.iter().enumerate().for_each(|(i, fileira)| {
+                    fileira.iter().enumerate().for_each(|(j, prateleira)| {
+                        prateleira.iter().enumerate().for_each(|(k, product)| {
+                            if product.is_some() {
+                                println!("{} {} {} -> {:?}", i, j, k, product.clone().unwrap());
+                            }
+                        })
+                    })
+                });
+                
+                
+                Ok("Showed")
+            }
+        }
+
+    }
+
+    fn help(&self){
+        println!("Usage:");
+        println!("   addProduct Fila Prateleira Zona product.id product.name product.expiration_date product.price product.quantity");
+        println!("   resupply product.id product.quantity");
+        println!("   removeProduct product.id product.quantity");
+        println!("   changePrice product.id product.new_price");
+        println!("   moveTo product.id Fila Prateleira Zona");
+        println!("   show");
+        println!();
+    }
+
+    fn read_command(&self, command: &str) -> Result<Command, &'static str> {
+        println!("Command: {}", command);
+        let mut splits = command.split_ascii_whitespace();
+        match splits.next().unwrap() {
+            "addProduct" => {
+                let fila = splits.next().unwrap_or("Expecting Fila");
+                let fila = fila.parse::<usize>().or(Err("Failed to parse 'Fila'"))?;
+
+                let prateleira = splits.next().unwrap_or("Expecting Prateleira");
+                let prateleira = prateleira.parse::<usize>().or(Err("Failed to parse 'Prateleira'"))?;
+
+                let zona = splits.next().unwrap_or("Expecting Zona");
+                let zona = zona.parse::<usize>().or(Err("Failed to parse 'Zona'"))?;
+
+                let product_id = splits.next().unwrap_or("Expecting Product ID");
+                let product_id = product_id.parse::<u32>().map_err(|_| "Invalid ID")?;
+
+                let product_name = splits.next().unwrap_or("Expecting Product Name");
+
+                let product_date = splits.next().unwrap_or("Expecting Product Expiration Date");
+
+                let product_price = splits.next().unwrap_or("Expecting Product Price");
+                let product_price = product_price.parse::<f64>().map_err(|_| "Invalid Price")?;
+
+                let product_quantity = splits.next().unwrap_or("Expecting Product Quantity");
+                let product_quantity = product_quantity.parse::<usize>().map_err(|_| "Invalid quantity")?;
+
+                Ok(Command::AddProduct(fila, prateleira, zona, Product { 
+                    id: product_id, 
+                    name: product_name.to_string(), 
+                    expiration: product_date.to_string(), 
+                    price: product_price,
+                    quantity: product_quantity 
+                }))
+            },
+            "resupply" => {
+                let product_id = splits.next().unwrap_or("Expecting Product ID");
+                let product_id = product_id.parse::<u32>().map_err(|_| "Invalid ID")?;
+                let product_quantity = splits.next().unwrap_or("Expecting Product Quantity");
+                let product_quantity = product_quantity.parse::<usize>().map_err(|_| "Invalid quantity")?;
+                Ok(Command::Resupply(product_id, product_quantity))
+            }
+            "removeProduct" => {
+                let product_id = splits.next().unwrap_or("Expecting Product ID");
+                let product_id = product_id.parse::<u32>().map_err(|_| "Invalid ID")?;
+                let product_quantity = splits.next().unwrap_or("Expecting Product Quantity");
+                let product_quantity = product_quantity.parse::<usize>().map_err(|_| "Invalid quantity")?;
+                Ok(Command::RemoveProduct(product_id, product_quantity))
+            },
+            "changePrice" => {
+                let product_id = splits.next().unwrap_or("Expecting Product ID");
+                let product_id = product_id.parse::<u32>().map_err(|_| "Invalid ID")?;
+                let product_price = splits.next().unwrap_or("Expecting Product Price");
+                let product_price = product_price.parse::<f64>().map_err(|_| "Invalid price")?;
+                Ok(Command::ChangePrice(product_id, product_price))
+            },
+            "moveTo" => {
+                let product_id = splits.next().unwrap_or("Expecting Product ID");
+                let product_id = product_id.parse::<u32>().map_err(|_| "Invalid ID")?;
+                let fila = splits.next().unwrap_or("Expecting Fila");
+                let fila = fila.parse::<usize>().or(Err("Failed to parse 'Fila'"))?;
+                let prateleira = splits.next().unwrap_or("Expecting Prateleira");
+                let prateleira = prateleira.parse::<usize>().or(Err("Failed to parse 'Prateleira'"))?;
+                let zona = splits.next().unwrap_or("Expecting Zona");
+                let zona = zona.parse::<usize>().or(Err("Failed to parse 'Zona'"))?;
+                Ok(Command::MoveTo(product_id, fila, prateleira, zona))
+            },
+            "show" => {
+                Ok(Command::Show())
+            }
+            _ => Err("Command Not valid"),
+        }
+    }
+}
+
 fn main() {
-    todo!("Implementar aqui todo o código para a mercearia")
+    let mut supermarket = Supermarket::new();
+    
+    let _ = supermarket.apply(Command::AddProduct(1,5,6, Product { id: 123, name: "test1".to_string(), expiration: "12/05/25".to_string(), price: 5.32, quantity: 1 }));
+    let _ = supermarket.apply(Command::AddProduct(6,2,4, Product { id: 234, name: "test2".to_string(), expiration: "13/06/26".to_string(), price: 15.32, quantity: 5 }));
+    let _ = supermarket.apply(Command::AddProduct(2,6,2, Product { id: 345, name: "test3".to_string(), expiration: "14/07/27".to_string(), price: 7.87, quantity: 2 }));
+    
+    loop{
+        supermarket.help();
+        let command = read_valid_stdin(">", |x| supermarket.read_command(x.trim()));
+        match supermarket.apply(command){
+            Ok(result) => println!("{}", result),
+            Err(result) => println!("{}", result),
+        }
+    }
+}
+
+fn read_valid_stdin<F, T>(prompt: &str, maplidator: F) -> T
+where
+    F: Fn(String) -> Result<T, &'static str>,
+{
+    loop {
+        let mut input = String::new();
+        print!("{}", prompt);
+        std::io::stdout().flush().unwrap();
+        std::io::stdin().read_line(&mut input).unwrap();
+        match maplidator(input) {
+            Ok(value) => break value,
+            Err(msg) => println!("{}", msg),
+        }
+    }
 }
